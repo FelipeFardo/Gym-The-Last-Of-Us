fc12e0fe05f5f2ca01d3cdc8b22c0bc67991131d
Felipe Fardo <felipe.fardo@hotmail.com.br>
2024-06-24 14:57:21 -0300

    feat(toy_text): add The Last Of Us

--- gym/envs/__init__.py
+++ gym/envs/__init__.py
@@ -115,6 +115,23 @@ register(
 )
 
 register(
+    id="TheLastOfUs-v1",
+    entry_point="gym.envs.toy_text.the_last_of_us:TheLastOfUsEnv",
+    kwargs={"map_name": "4x4"},
+    max_episode_steps=100,
+    reward_threshold=0.70,  # optimum = 0.74
+)
+
+register(
+    id="TheLastOfUs8x8-v1",
+    entry_point="gym.envs.toy_text.the_last_of_us:TheLastOfUsEnv",
+    kwargs={"map_name": "8x8"},
+    max_episode_steps=200,
+    reward_threshold=0.85,  # optimum = 0.91
+)
+
+
+register(
     id="CliffWalking-v0",
     entry_point="gym.envs.toy_text.cliffwalking:CliffWalkingEnv",
 )
--- gym/envs/toy_text/__init__.py
+++ gym/envs/toy_text/__init__.py
@@ -1,4 +1,5 @@
 from gym.envs.toy_text.blackjack import BlackjackEnv
 from gym.envs.toy_text.cliffwalking import CliffWalkingEnv
 from gym.envs.toy_text.frozen_lake import FrozenLakeEnv
+from gym.envs.toy_text.the_last_of_us import TheLastOfUsEnv
 from gym.envs.toy_text.taxi import TaxiEnv
